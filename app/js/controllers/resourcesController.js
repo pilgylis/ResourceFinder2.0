beff7d5646e6080646440f55e473153ba94f7dde
pilgylis <nguyen.viettrung175@gmail.com>
2013-09-26 13:13:51 +0700

    Continuing with the Resources page

--- app/js/controllers/resourcesController.js
+++ app/js/controllers/resourcesController.js
@@ -1,5 +1,20 @@
 define(['app', "services/resourceService"], function (app) {
     app.lazy.controller("resourcesController", ['$scope', 'resourceService', function ($scope, resourceService) {
-        $scope.resources = resourceService.query({page: 0});
+        $scope.resources = resourceService.query({ page: 0 });
+
+        $scope.timeslots = new Array(24);
+
+        var today = new Date();
+        var getDay = function (date) {
+            var dd = date.getDate();
+            var mm = date.getMonth() + 1; //January is 0!
+
+            var yyyy = date.getFullYear();
+            return dd + '/' + mm + '/' + yyyy;
+        };
+
+        var tomorrow = new Date();
+        tomorrow.setDate(today.getDate() + 1);
+        $scope.days = [getDay(today)];
 	}]);
 });
